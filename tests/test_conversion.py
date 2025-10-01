7fb2cdc97da7f23f9819ea072bc421c9c27d7f1e
Louis Beal <louis.j.beal@gmail.com>
2025-10-01 16:55:02 +0200

    Add model conversion testing

---
+++ tests/test_conversion.py
@@ -0,0 +1,51 @@
+import json
+import pytest
+
+from pydantic import BaseModel, Field
+from ..agent.Onto_wa_rag.json_coerce.model_convert import convert_model_to_struct
+
+
+class SimpleModel(BaseModel):
+    name: str
+
+
+class LargeModel(BaseModel):
+    name: str
+    age: int
+    height: float
+    is_student: bool
+    hobbies: list[str]
+
+class FieldModel(BaseModel):    
+    thought: str = Field(..., description="My thoughts on the current situation.")
+
+
+translate = {
+    "str": "string",
+    "int": "integer",
+    "float": "number",
+    "bool": "boolean",
+    "list": "array",
+    "dict": "object",
+}
+
+
+@pytest.mark.parametrize("model", [SimpleModel, LargeModel])
+def test_model_conversion(model: BaseModel.__class__) -> None:
+    structure = convert_model_to_struct(model)
+    assert structure.startswith("{")
+    assert structure.endswith("}")
+
+    clean = "\n".join(line for line in structure.split("\n") if "//" not in line)
+
+    as_dict = json.loads(clean)
+
+    for name, field in model.model_json_schema().get("properties", {}).items():
+        assert name in as_dict
+        assert translate[as_dict[name]] == field.get("type")
+
+
+def test_field_model() -> None:
+    structure = convert_model_to_struct(FieldModel)
+
+    assert "My thoughts on the current situation." in structure
